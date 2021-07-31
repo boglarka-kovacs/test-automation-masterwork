6dc0bfc53f070ff02270a330ddf259509ea6c6e7
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-07-31 13:27:18 +0200

    Add allure annotations to TC01_SignUpIsFailTest

--- src/test/java/masterwork/myStore/TC02_SignUpIsFailTest.java
+++ src/test/java/masterwork/myStore/TC02_SignUpIsFailTest.java
@@ -1,13 +1,23 @@
 package masterwork.myStore;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.SignUpPage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@Epic("Your Store User Handling")
+@Feature("Signup Feature")
+@Story("Failed signup with missing required email.")
 public class TC02_SignUpIsFailTest extends BaseTest{
 
   @Test
+  @DisplayName("Signup failed")
+  @Description("Test for signup with missing email")
   public void signUpIsFailed() {
     HomePage homePage = new HomePage(driver);
 
