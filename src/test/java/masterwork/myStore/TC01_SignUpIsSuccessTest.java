2039a490b0c5ce8d71eae54d3967d9ad067204f4
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-07-31 13:20:46 +0200

    Add allure annotations to TC01_SignUpIsSuccessTest

--- src/test/java/masterwork/myStore/TC01_SignUpIsSuccessTest.java
+++ src/test/java/masterwork/myStore/TC01_SignUpIsSuccessTest.java
@@ -1,13 +1,23 @@
 package masterwork.myStore;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.SignUpPage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@Epic("Your Store User Handling")
+@Feature("Signup Feature")
+@Story("Successfully signup with correct required dates.")
 public class TC01_SignUpIsSuccessTest extends BaseTest{
 
   @Test
+  @DisplayName("Signup success")
+  @Description("Test for signup with valid dates")
   public void signUpIsSuccessful(){
     HomePage homePage = new HomePage(driver);
     SignUpPage signup = new SignUpPage(driver);
