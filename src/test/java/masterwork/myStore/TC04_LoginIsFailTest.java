d7ad779272dac0e5ab8cb6ccd85ab9809e0feb4c
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-07-31 13:28:58 +0200

    Add allure annotations to TC04_LoginIsFailTest

--- src/test/java/masterwork/myStore/TC04_LoginIsFailTest.java
+++ src/test/java/masterwork/myStore/TC04_LoginIsFailTest.java
@@ -1,13 +1,23 @@
 package masterwork.myStore;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.LoginPage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@Epic("Your Store User Handling")
+@Feature("login Feature")
+@Story("Failed login with correct username and incorrect password")
 public class TC04_LoginIsFailTest extends BaseTest{
 
   @Test
+  @DisplayName("Login failed")
+  @Description("Login to Your Store with valid user, invalid pass")
   public void loginIsFail() {
     HomePage homePage = new HomePage(driver);
     LoginPage loginPage = new LoginPage(driver);
