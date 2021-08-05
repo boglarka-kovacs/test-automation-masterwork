12f96d18c7c98d5278ad235082deeb01953cba59
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-08-05 12:16:20 +0200

    Refactored TC01_SignUpIsSuccessTest

--- src/test/java/masterwork/myStore/TC01_SignUpIsSuccessTest.java
+++ src/test/java/masterwork/myStore/TC01_SignUpIsSuccessTest.java
@@ -1,12 +1,12 @@
 package masterwork.myStore;
 
+import static org.assertj.core.api.Assertions.*;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.SignUpPage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,20 +14,26 @@ import org.junit.jupiter.api.Test;
 @Feature("Signup Feature")
 @Story("Successfully signup with correct required dates.")
 public class TC01_SignUpIsSuccessTest extends BaseTest{
+  HomePage homePage = new HomePage(driver);
+  SignUpPage signup = new SignUpPage(driver);
 
   @Test
   @DisplayName("Signup success")
   @Description("Test for signup with valid dates")
   public void signUpIsSuccessful(){
-    HomePage homePage = new HomePage(driver);
-    SignUpPage signup = new SignUpPage(driver);
 
+    LOG.info("Start test = signup");
     homePage.open();
+
+    LOG.info("Open signup page");
     homePage.navigateToRegistration();
 
-    signup.signUp("Teszt", "Elek", "teszt11@example.com",
+    LOG.info("Called signup page.signup");
+    signup.signUp("Teszt", "Elek", "teszt31@example.com",
             "122654398", "successTry23", "successTry23");
 
-    Assertions.assertThat(driver.getTitle()).isEqualTo("Your Account Has Been Created!");
+    LOG.info("Check signup success page is loaded");
+    assertThat(driver.getTitle()).isEqualTo("Your Account Has Been Created!");
+    makeScreenshot(driver);
   }
 }
