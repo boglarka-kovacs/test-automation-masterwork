df70c0de2a6c91b889af0864a7f4e45db90493e3
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-08-05 12:24:52 +0200

    Refactored TC08_SendProductReviewSuccessTest

--- src/test/java/masterwork/myStore/TC08_SendProductReviewSuccessTest.java
+++ src/test/java/masterwork/myStore/TC08_SendProductReviewSuccessTest.java
@@ -1,38 +1,41 @@
 package masterwork.myStore;
 
-
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.MacBookPage;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-
 @Epic("Your Store Product Handling")
 @Feature("Product Review Feature")
-@Story("Successfully send a review for the product ")
+@Story("Successfully send a review for the product")
 public class TC08_SendProductReviewSuccessTest extends BaseTest{
+  HomePage homePage = new HomePage(driver);
+  MacBookPage review = new MacBookPage(driver);
 
   @Test
   @DisplayName("Product review success")
   @Description("Test for successful sending review for the product")
   public void sendReviewToProduct() {
-    HomePage homePage = new HomePage(driver);
-    MacBookPage review = new MacBookPage(driver);
+
+    LOG.info("Start test=review");
     homePage.open();
-    homePage.getProductImg.click();
+    homePage.getProductImg().click();
 
-    Assertions.assertThat(driver.getTitle()).isEqualTo("MacBook");
+    LOG.info("Check product page is loaded");
+    assertThat(driver.getTitle()).isEqualTo("MacBook");
 
+    LOG.info("Write review to product");
     review.sendReview("Teszt Elek", "It is an awesome product. I really recommend it.");
-    wait.until(ExpectedConditions.visibilityOf(review.getThankYouMessage));
+    wait.until(ExpectedConditions.visibilityOf(review.getGetThankYouMessage()));
 
-    Assertions.assertThat(review.getThankYouMessage.getText())
+    LOG.info("Check review was sending");
+    assertThat(review.getGetThankYouMessage().getText())
             .isEqualTo("Thank you for your review. It has been submitted to the webmaster for approval.");
   }
 }
