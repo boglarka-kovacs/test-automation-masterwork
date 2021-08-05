39b278f1d5ac7ee12f992d13e04d6f37b984392e
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-08-05 12:06:47 +0200

    Refactored BasePage

--- src/test/java/masterwork/myStore/Pages/BasePage.java
+++ src/test/java/masterwork/myStore/Pages/BasePage.java
@@ -1,13 +1,17 @@
 package masterwork.myStore.Pages;
 
+import masterwork.myStore.BaseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasePage {
 
   protected final WebDriver driver;
   protected final WebDriverWait wait;
+  protected final Logger LOG = LoggerFactory.getLogger(BasePage.class);
 
   public BasePage(WebDriver driver) {
     this.driver = driver;
