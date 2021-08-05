be28d5ab964f97ce63d952a05aa17040c83df79a
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-08-05 12:24:19 +0200

    Refactored TC07_PaginationSuccessfulTest

--- src/test/java/masterwork/myStore/TC07_PaginationSuccessfulTest.java
+++ src/test/java/masterwork/myStore/TC07_PaginationSuccessfulTest.java
@@ -1,33 +1,40 @@
 package masterwork.myStore;
 
+import static org.assertj.core.api.Assertions.*;
+
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import masterwork.myStore.Pages.AllDesktopProductsPage;
 import masterwork.myStore.Pages.HomePage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @Epic("Your Store Product Handling")
-@Feature("Product Listing Feature")
+@Feature("Product listing Feature")
 @Story("Successfully list more then one page of products")
 public class TC07_PaginationSuccessfulTest extends BaseTest{
+  HomePage homePage = new HomePage(driver);
+  AllDesktopProductsPage productPage = new AllDesktopProductsPage(driver);
 
   @Test
   @DisplayName("Product listing success")
   @Description("Test for successful multi-page listing of products")
   public void listingProductsSecondPage() {
-    HomePage homePage = new HomePage(driver);
-    AllDesktopProductsPage productPage = new AllDesktopProductsPage(driver);
 
+    LOG.info("Start test=pagination");
     homePage.open();
-    homePage.desktopsButton.click();
-    homePage.allDesktopsLink.click();
+    homePage.getDesktopsButton().click();
+
+    LOG.info("Open all desktop page");
+    homePage.getAllDesktopsLink().click();
+    makeScreenshot(driver);
     productPage.clickTheNextPage();
 
-    Assertions.assertThat(driver.getCurrentUrl())
+    LOG.info("Check second page is loaded");
+    assertThat(driver.getCurrentUrl())
             .isEqualTo("http://test-automation-shop2.greenfox.academy/desktops?page=2");
+    makeScreenshot(driver);
   }
 }
