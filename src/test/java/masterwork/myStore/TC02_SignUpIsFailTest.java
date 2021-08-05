07ea3a3e7ff067960a62fc9d2dc5321cf9709e3f
boglarka-kovacs <boglaarka.kovacs@gmail.com>
2021-08-05 12:17:08 +0200

    Refactored and modified TC02_SignUpIsFailTest

--- src/test/java/masterwork/myStore/TC02_SignUpIsFailTest.java
+++ src/test/java/masterwork/myStore/TC02_SignUpIsFailTest.java
@@ -1,34 +1,51 @@
 package masterwork.myStore;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import masterwork.myStore.Pages.HomePage;
 import masterwork.myStore.Pages.SignUpPage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 @Epic("Your Store User Handling")
 @Feature("Signup Feature")
-@Story("Failed signup with missing required email.")
-public class TC02_SignUpIsFailTest extends BaseTest{
+@Story("Failed signup with wrong characters of first name")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TC02_SignUpIsFailTest extends BaseTest {
 
-  @Test
-  @DisplayName("Signup failed")
-  @Description("Test for signup with missing email")
-  public void signUpIsFailed() {
+  @BeforeAll
+  @Step("Open signup page")
+  @Description("Open homepage and navigate to signup page")
+  public void openSignUpPage() {
     HomePage homePage = new HomePage(driver);
 
+    LOG.info("Start test = signup");
     homePage.open();
-    homePage.navigateToRegistration();
 
+    LOG.info("Open signup page");
+    homePage.navigateToRegistration();
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/failedSignupBoundaryValues.csv", numLinesToSkip = 1)
+  @DisplayName("Signup failed")
+  @Feature("Signup Feature")
+  @Description("Test for signup with with boundary value of the required fields")
+  public void signUpIsFailed(String firstName, String lastName, String email, String phone, String passw, String confirm) {
     SignUpPage signup = new SignUpPage(driver);
 
-    signup.signUp("Teszt", "Etel", "",
-            "122654398", "failTry23", "failTry23");
+    LOG.info("Called signup page.signup");
+    signup.signUp(firstName, lastName, email, phone, passw, confirm);
 
-    Assertions.assertThat(signup.isError()).isTrue();
+    LOG.info("Check failed signup message is loaded");
+    Assertions.assertThat(signup.isFirstNameError() || signup.isLastNameError() ||
+            signup.isEmailError() || signup.isPhoneError() || signup.isPasswordError() ||
+            signup.isConfirmError()).isTrue();
+
+    makeScreenshot(driver);
+    signup.signUpFieldClear();
   }
 }
